17ac49a3e195a37339601192d03c0db04d4bff59
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-12-10 18:13:34 -0600

    Fix indentation

--- constrainingconductor/moveThese/permeabilitySims.py
+++ constrainingconductor/moveThese/permeabilitySims.py
@@ -41,39 +41,39 @@ for sweep in range(sweepStart, sweepStart+n_sweeps):
         master.grofile = originalGrofile
         sim_folder = 'sweep{}/Sim{}'.format(sweep,sim)
         p = subprocess.Popen('mkdir -p {}'.format(sim_folder), shell=True, 
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p.wait()
         os.chdir(sim_folder)
         master.writeTracers('tracers.out')
         master.writeWindows('z_windows.out')
         master.writeWindows(os.path.join(baseDir, 'sweep{}/z_windows.out'.format(sweep)))
-        
+
 
         for stage in range(1,6):
             stageInformation = { 
-                1: {'filename':"Stage1_Weak{}".format(sim), 'pull_coord_k':40, 
-                    'simWindows':master.windows[sim]*np.ones(master._n_tracers)}, 
-                2: {'filename':"Stage2_Strong{}".format(sim), 'pull_coord_k':500, 
-                    'simWindows':master.windows[sim]*np.ones(master._n_tracers)}, 
-                3: {'filename':"Stage3_Moving{}".format(sim), 
-                    'pull_coord_k':500, 'moving':True, 'P':None,
-                    'simWindows': master.windows[sim::master.n_sims]
-                    }, 
-                4: {'filename':"Stage4_Eq{}".format(sim), 'pull_coord_k':1000, 
-                    'simWindows': master.windows[sim::master.n_sims]},
-                5: {'filename':"Stage5_ZCon{}".format(sim), 'pull_coord_k':1000, 
-                    'pull_nstfout':int(0.01/dt), 
-                    'simWindows': master.windows[sim::master.n_sims]
-                    } 
-                }
+                    1: {'filename':"Stage1_Weak{}".format(sim), 'pull_coord_k':40, 
+                        'simWindows':master.windows[sim]*np.ones(master._n_tracers)}, 
+                    2: {'filename':"Stage2_Strong{}".format(sim), 'pull_coord_k':500, 
+                        'simWindows':master.windows[sim]*np.ones(master._n_tracers)}, 
+                    3: {'filename':"Stage3_Moving{}".format(sim), 
+                        'pull_coord_k':500, 'moving':True, 'P':None,
+                        'simWindows': master.windows[sim::master.n_sims]
+                        }, 
+                    4: {'filename':"Stage4_Eq{}".format(sim), 'pull_coord_k':1000, 
+                        'simWindows': master.windows[sim::master.n_sims]},
+                    5: {'filename':"Stage5_ZCon{}".format(sim), 'pull_coord_k':1000, 
+                        'pull_nstfout':int(0.01/dt), 
+                        'simWindows': master.windows[sim::master.n_sims]
+                        } 
+                    }
 
             if stage==5 and stage5_lmps:
-               force_indices = [sim + int(i*len(master.windows)/master.n_tracers) 
-                    for i in range(master.n_tracers)]
+                force_indices = [sim + int(i*len(master.windows)/master.n_tracers) 
+                        for i in range(master.n_tracers)]
                 lmpsUtils.lmps_conversion(stageInformation[4]['filename']+".gro",
                         master.windows[sim::master.n_sims],
                         master.tracers, sim)
-                
+
                 master.lmprun(stageInformation[5]['filename']+'_lmps',
                         'Stage5_ZCon.input')
 
